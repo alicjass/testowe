5dd00db5c483b50c26a18b085b59455a73dbf962
Alicja Serafinska <aserafinska1@st.swps.edu.pl>
2025-12-05 09:52:07 +0100

    lab_08_continued

--- gr_3_project/biblioteka/views.py
+++ gr_3_project/biblioteka/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, redirect
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Book, Osoba, Stanowisko
 from .serializers import BookSerializer, OsobaSerializer, StanowiskoSerializer
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from django.http import Http404
+from .forms import OsobaForm
 
 # określamy dostępne metody żądania dla tego endpointu
 @api_view(['GET', "POST"])  # dekorator zawsze przed funkcją
@@ -177,9 +178,13 @@ def osoba_detail_html(request, id):
     except Osoba.DoesNotExist:
         raise Http404("Obiekt Osoba o podanym id nie istnieje")
 
-    return render(request,
-                  "biblioteka/osoba/detail.html",
-                  {'osoba': osoba})
+    if request.method == "GET":
+        return render(request,
+                    "biblioteka/osoba/detail.html",
+                    {'osoba': osoba})
+    if request.method == "POST":
+        osoba.delete()
+        return redirect('osoba-list') 
 
 
 def osoba_create_html(request):
@@ -212,3 +217,17 @@ def osoba_create_html(request):
         else:
             error = "Wszystkie pola są wymagane."
             return render(request, "biblioteka/osoba/create.html", {'error': error, 'stanowiska': stanowiska})
+
+
+def osoba_create_django_form(request):
+    if request.method == "POST":
+        form = OsobaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('osoba-list')  
+    else:
+        form = OsobaForm()
+
+    return render(request,
+                  "biblioteka/osoba/create_django.html",
+                  {'form': form})
