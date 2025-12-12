6aca6bff8a857a6a3bbb365bc8af9a7ed4a50aed
Alicja Serafinska <aserafinska1@st.swps.edu.pl>
2025-12-12 10:04:53 +0100

    lab_09

--- gr_3_project/biblioteka/urls.py
+++ gr_3_project/biblioteka/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('books/', views.book_list),
     path('books/<int:pk>/', views.book_detail),
+    path('books/update_delete/<int:pk>/', views.book_update_delete),
     path('osoby/', views.osoba_list),
     path('osoby/<int:pk>/', views.osoba_detail),
     path('osoby/nazwisko/<str:name>/', views.osoba_name_filter_url),
@@ -15,4 +16,8 @@ urlpatterns = [
     path("html/osoby/", views.osoba_list_html, name="osoba-list"),
     path("html/osoby/<int:id>/", views.osoba_detail_html, name="osoba-detail"),
     path("html/osoby/dodaj/", views.osoba_create_html, name="osoba-create"),
+    path('login/', views.user_login, name='user-login'),
+    path('logout/', views.user_logout, name='user-logout'),
+    path('token/login/', views.drf_token_login, name='drf-token-login'),
+    path('token/logout/', views.drf_token_logout, name='drf-token-logout'),
 ]
